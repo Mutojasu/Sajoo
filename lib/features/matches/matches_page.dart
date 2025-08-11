8383aabc446cacaaaaa371dae16a60e778d1bd38
Mutojasu <hjkim1238@gmail.com>
2025-08-11 12:46:26 +0900

    chore: add matches placeholder

---
+++ lib/features/matches/matches_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class MatchesPage extends StatelessWidget {
+  const MatchesPage({super.key});
+  @override Widget build(BuildContext c)=>Scaffold(
+    appBar: AppBar(title: const Text('Matches')),
+    body: const Center(child: Text('매치 목록(추가 예정)')),
+  );
+}
